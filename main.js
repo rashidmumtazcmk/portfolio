06613adb2d9f6f2faaa0d7dae519c9df8fe69092
rashidmumtazcmk <rashidmumtazcmk@gmail.com>
2026-06-20 12:55:43 -0700

    1.0

---
+++ main.js
@@ -0,0 +1,12 @@
+AOS.init();
+
+new Typed("#typing", {
+    strings: [
+        "Junior Unity Developer",
+        "Gameplay Programmer",
+        "Tool Developer"
+    ],
+    typeSpeed: 60,
+    backSpeed: 40,
+    loop: true
+});
